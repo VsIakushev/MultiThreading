45b9cbeed8007e8ae3133052fdb3083d99718595
VsIakushev <vsiakushev@gmail.com>
2024-03-18 13:40:36 -0700

    Init

---
+++ MultiThreading/MultiThreading/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MultiThreading
+//
+//  Created by Vitaliy Iakushev on 18.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
